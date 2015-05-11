6551fda97876a05bb62be552647e56a446f6b02f
caiodimov <change2anew1>
2015-05-10 22:38:06 -0500

    hw6

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,17 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+  	p=Photo.show(params[:id]) 
+
+  end	
+
+def update_row
+p = Photo.find(params[:id])
+p.source = params[:the_source]
+p.caption = params[:the_caption]
+p.save
+end
+
 
 end
